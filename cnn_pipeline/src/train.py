b7694a77e19320526d05db4a007ccaace9b89dc2
Hesch <heschmat.seyedi@gmail.com>
2024-06-06 20:15:04 +0200

    Add function to validate at the end of training for one epoch.

--- cnn_pipeline/src/train.py
+++ cnn_pipeline/src/train.py
@@ -35,3 +35,31 @@ def train_one_epoch(trn_dataloader, model, optimizer, criterion):
 
     return trn_loss
 
+
+def valid_one_epoch(val_dataloader, model, criterion):
+    """
+    Validate at the end of one eopoch training.
+    """
+    cuda_available = torch.cuda.is_available()
+    device = torch.device('cuda' if cuda_available else 'cpu')
+    if cuda_available:
+        model.to(device)
+
+    with torch.inference_mode():
+        model.eval()
+        val_loss = 0.0
+
+        for batch_idx, (images, labels) in tqdm(enumerate(val_dataloader, start= 1),
+                                                desc= 'Validating',
+                                                total= len(val_dataloader),
+                                                leave= True,
+                                                ncols= 80):
+            if cuda_available:
+                images, labels = images.to(device), labels.to(device)
+            logits = model(images)
+            loss = criterion(logits, labels)
+            # Check `train_one_epoch` for logic:
+            val_loss += (1 / batch_idx) * (loss.data.item() - val_loss)
+        return val_loss
+
+
